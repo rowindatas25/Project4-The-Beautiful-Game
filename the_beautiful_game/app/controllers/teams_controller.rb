542f3d72e71659dad14dc9a7d529571f60a79956
Rohtas Wadera <rohtaswadera@ool-ad0200a0.dyn.optonline.net>
2017-11-14 16:29:33 -0500

    index and show views are set up and working with dummy data

--- the_beautiful_game/app/controllers/teams_controller.rb
+++ the_beautiful_game/app/controllers/teams_controller.rb
@@ -4,42 +4,58 @@ class TeamsController < ApplicationController
 	
 	def index
 		@teams = Team.all
-		render json: @teams
+		
 	end
 
 	def show
 		@team = Team.find(params[:id])
-		render json: @team
+		
 	end
 
 	def new
-		
+		@team = Team.new
 	end
 
 	def create
-		
+		@team = Team.new(team_params)
+
+		if @team.save
+			redirect_to @team
+		else
+			render :action => new
+		end
 	end
 
+
 	def edit
-		
+		@team = Team.find(params[:id])
 	end
 
 	def update
-		
+		@team = Team.find(params[:id])
+
+		if @team.update(team_params)
+			redirect_to @team
+		else
+			render :action => :edit
+		end
 	end
 
+
 	def destroy
-		
+		@team = Team.find(params[:id])
+		@team.destroy
+		redirect_to teams_url
 	end
 
 	private
 
 	def team_params
-		
+		params.require(:team).permit(:team, :team_id, :flag, :founded, :moments)
 	end
 
 	def set_team
-		
+		@team = Team.find(params[:id])
 	end
 
 end
